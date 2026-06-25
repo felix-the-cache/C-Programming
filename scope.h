dfa7dbf3d11154c39abdfbe0e65aac46f38930f0
felix-the-cache <felixvjr0@gmail.com>
2026-06-24 23:22:30 -0400

    Update scope.h

--- scope.h
+++ scope.h
@@ -4,9 +4,10 @@ int sample(int arg1, int arg2) {
   int loc1, loc2, res1, res2, res3;
   loc1 = 100;
   loc2 = 200;
+  // Static variables allow you to retain data until the parent caller function ends -- in this case the main() function.
   static int stat1 = 0;
   res1 = arg1 + arg2;
-
+// block level variable.
   {
     printf("Function local variable: %d", loc1);
     int loc3 = 10;
