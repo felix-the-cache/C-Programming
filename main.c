9803f2390a515d3b5add8cae035ce95163641a05
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2026-07-01 15:38:37 -0400

    setting up some arrays

--- main.c
+++ main.c
@@ -10,4 +10,17 @@ int main(void) {
 
   swap_pointer(&a, &b);
   printf("main_pointer: a = %d, b = %d\n", a, b);
+
+  char z[5];
+  z[0] = 'C';
+  printf("z[0] = %c\n", z[0]);
+
+  int *p;
+  int f[9];
+  p = &f[0];
+  *p = 100;  // same as f[0] = 100;
+  p = p + 1; // setting *p to the next element in the array. -> f[1]
+  *p = 200;  // setting f[1] to 200
+  printf("P is -> %d\n", f[0]);
+  printf("p[1] is -> %d\n", f[1]);
 }
