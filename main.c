5449f711eba0bdeeeed5b15aed9d16b4a33c199a
Felix Vargas Jr <109393467+NeverGiveUp23@users.noreply.github.com>
2026-07-01 16:37:17 -0400

    setting up some arrays

--- main.c
+++ main.c
@@ -23,4 +23,40 @@ int main(void) {
   *p = 200;  // setting f[1] to 200
   printf("P is -> %d\n", f[0]);
   printf("p[1] is -> %d\n", f[1]);
+
+  int s[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int *o = s;
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", o[i]);
+  }
+
+  o[1] = 200;
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", o[i]);
+  }
+
+  for (int i = 9; i >= 0; i--) {
+    printf("%d\n", s[i]);
+  }
+
+  int i = 0;
+  int j = 0;
+  while (i < 20) {
+    printf("%d\n", s[j]);
+    i = i + 1;
+    j = (j + 1) % 10;
+  }
+
+  int test[8];
+  i = 7;
+  while (i != 1) {
+    printf("%d\n", s[i]);
+    i = i / 2;
+  }
+
+  i = 0;
+  while (i < 10) {
+    printf("%d\n", s[i]);
+    i = 2 * (i + 1);
+  }
 }
