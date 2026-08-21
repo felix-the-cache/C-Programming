21ebc7a1171dbd5455205c47ab11265b7c493de8
felix-the-cache <felixvjr0@gmail.com>
2026-08-20 23:21:31 -0400

    finishing up

---
+++ structs.h
@@ -0,0 +1,14 @@
+
+struct birdStruct {
+  char const *jay;
+  char const *magpie;
+  char const *raven;
+  char const *chough;
+};
+
+struct birdStruct const aName{
+    .jay = "Lissy",
+    .magpie = "Popper",
+    .raven = "Frau",
+    .chough = "Henry",
+};
